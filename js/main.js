48fe38a5bf6f90b2265d6dcd3dc4c4c844874a59
unexpectedhaki <tpritchard843@gmail.com>
2023-06-22 13:16:55 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch);
+
+document.querySelector('h2').innerText = localStorage.getItem('books');
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  // 9780385540735 --> Surfing with Sartre
+  // 9780345391803 --> Hitchhiker's Guide to the Galaxy
+  const url = `https://openlibrary.org/isbn/${choice}.json`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data.title);
+        // if localStorage is empty, add first book into local storage
+        if (!localStorage.getItem('books')) {
+          localStorage.setItem('books', data.title);
+        } else {
+          let books = localStorage.getItem('books') + ';' + data.title;
+          localStorage.setItem('books', books);
+        }
+        //place books in DOM
+        document.querySelector('h2').innerText = localStorage.getItem('books');
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
